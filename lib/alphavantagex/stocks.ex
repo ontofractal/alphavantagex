48eb3024ea7e2317ac8b13948ff316e3f8a3e7d9
ontofractal <ontofractal@protonmail.com>
2018-10-11 16:20:53 +0100

    add fetch_ts_daily_adj fn and test

--- lib/alphavantagex/stocks.ex
+++ lib/alphavantagex/stocks.ex
@@ -1,4 +1,19 @@
 defmodule Alphavantagex.Stocks do
+
   use Tesla, docs: false, only: ~w(get)a
+
+  def fetch_ts_daily_adj(symbol) do
+    client = Alphavantagex.client()
+
+    result =
+      get(client, "/",
+        query: [function: "TIME_SERIES_DAILY_ADJUSTED", symbol: symbol, datatype: "csv"]
+      )
+
+    with {:ok, tesla_env} <- result do
+
+      {:ok, tesla_env}
+    end
+  end
 
 end
--- test/alphavantagex_test.exs
+++ test/alphavantagex_test.exs
@@ -2,7 +2,4 @@ defmodule AlphavantagexTest do
   use ExUnit.Case
   doctest Alphavantagex
 
-  test "greets the world" do
-    assert Alphavantagex.hello() == :world
-  end
 end
