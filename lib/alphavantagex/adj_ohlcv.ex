b5b84b22ff4bc4100f9faa5e5f6e100d935df550
ontofractal <ontofractal@protonmail.com>
2018-10-12 15:22:45 +0100

    add datetime field to AdjOHLCV struct

--- lib/alphavantagex/adj_ohlcv.ex
+++ lib/alphavantagex/adj_ohlcv.ex
@@ -8,17 +8,25 @@ defmodule Alphavantagex.AdjOHLHCV do
     field :open, :float
     field :split_coefficient, :float
     field :timestamp, :integer
+    field :datetime, :naive_datetime
     field :volume, :float
   end
 
   def parse!(raw) do
     raw
     |> Map.update!(:timestamp, &datestring_to_timestamp/1)
+    |> Map.update!(:datetime, &datestring_to_ndt/1)
   end
 
   def datestring_to_timestamp(str) do
     str
     |> Date.from_iso8601!()
     |> Timex.to_unix()
+  end
+
+  def datestring_to_ndt(str) do
+    str
+    |> Date.from_iso8601!()
+    |> Timex.to_datetime()
   end
 end
